bd03f317727ef6be8f40274f3a9bb2e79432a1a4
Clay-Rasmussen <clayrasmussen425@gmail.com>
2026-04-16 17:42:21 -0500

    Add Flappy Bird game implementation

---
+++ FlappyBird.js
@@ -0,0 +1,81 @@
+let bird;
+let pipes;
+let distBetween;
+let nextSpawn;
+ 
+function setup() {
+  createCanvas(600, 400);
+  distBetween = width / 3;
+  nextSpawn = random(distBetween, width - width / 4);
+  pipes = [new Pipe()];
+  bird = new Bird(64, height / 2);
+}
+ 
+function draw() {
+  background('lightblue');
+  if (pipes.length <= 0 || width - pipes[pipes.length -1].x >= nextSpawn) {
+    pipes.push(new Pipe());
+  }
+  for (let i = pipes.length -1; i>=0; i--) {
+    pipes[i].update();
+    pipes[i].show();
+  }
+  bird.update();
+  bird.show();
+}
+ 
+function keyPressed() {
+  if (key == ' ') {
+    bird.flap();
+  }
+}
+ 
+class Bird {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.velocity = 0;
+    this.gravity = 0.8;
+    this.lift = -15;
+  }
+  show () {
+    fill('yellow');
+    ellipse(this.x, this.y, 32);
+  }
+  update() {
+    this.velocity += this.gravity;
+    this.velocity *= 0.9;
+    this.y += this.velocity;
+    if (this.y <0) {
+      this.y =0;
+      this.velocity =0;
+    }
+    if (this.y > height) {
+      this.y = height;
+      this.velocity = 0;
+    }
+  }
+  flap() {
+    this.velocity += this.lift;
+  }
+}
+ 
+class Pipe {
+  constructor(){
+    this.spacing = 140;
+    this.top = random(height / 6, height / 2);
+    this.bottom = height - (this.top + this.spacing)
+    this.x = width;
+    this.width = 60;
+    this.speed = 3;
+    this.pastBird = false;
+  }
+  show() {
+    fill('green');
+    rect(this.x, 0, this.width, this.top);
+    rect(this.x, height - this.bottom, this.width, this.bottom);    
+  }
+  update() {
+    this.x -= this.speed;
+  }
+}
